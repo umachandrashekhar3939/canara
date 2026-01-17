91c090a0b9fe77bf86a1342941f70b3157b3598c
umachandrashekhar3939 <umachandrashekhar3939@gmail.com>
2026-01-17 21:45:01 +0530

    Update App.java

--- src/main/java/cognizant/App.java
+++ src/main/java/cognizant/App.java
@@ -6,6 +6,6 @@ package cognizant;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("hello world");
+        System.out.println("hello world")
     }
 }
